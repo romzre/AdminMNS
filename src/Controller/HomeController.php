e4125a7fc9aab0c04fe6122d0aad8c92ccba5b0a
romzre <albrogio@gmail.com>
2022-04-29 16:10:59 +0200

    fix register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -46,14 +46,13 @@ class HomeController extends Controller {
             $data = compact('samePass','message','email');
             
         }
-<<<<<<< HEAD
+
         require '../src/Entity/Form.php';
         $form = new Form();
         $data['form'] = $form;
         $data['POST'] = $_POST;
         $data['messageEndCheck']=$messageEndCheck;
-=======
->>>>>>> adab1e25e043f587131cda4221f4491ca206f178
+
         $path = 'pages/home/register.html.twig';
         $layOut='base';
         $this->renderView($path, $data, $layOut);
