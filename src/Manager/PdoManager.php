540349a30b082cbf8132458ea53c14b02254110f
romzre <albrogio@gmail.com>
2022-05-03 15:38:18 +0200

    Fix PDO

--- src/Manager/PdoManager.php
+++ src/Manager/PdoManager.php
@@ -12,7 +12,8 @@
         {
             try{
 
-                self::$pdo = new PDO('mysql:host=88.166.155.219:3306;dbname=adminMns','kyoko9273','Spedum1463!');
+                self::$pdo = new PDO('mysql:host=localhost:3306;dbname=adminMns','kyoko9273','Spedum1463!');
+                
 
             }
             catch (PDOException $e)
