cbb87da4b82b4905b487d36d44079ec17e614e51
Amelie Le Mestre <amelie.le.mestre@gmail.com>
2022-06-09 09:51:33 +0200

    modif lien email reset password

--- app/service/forgot-password.php
+++ app/service/forgot-password.php
@@ -34,31 +34,33 @@ if (!$email) {
         );
         $expDate = date("Y-m-d H:i:s", $expFormat); // on récupère la date d'expiration de la clé en faisant la date et l'heure actuelle + 1h
         $key = bin2hex(random_bytes(32)); //genere un tolken de 32 caractères
-       
+
         // on insère le tolken unique,  la date d'expiration et l'email de user dans la table password-reset-temp 
         require '../src/Manager/PasswordResetManager.php';
         $passwordResetManager = new PasswordResetManager();
-        $passwordResetManager->createPasswordResetTemp($email,$key,$expDate);
-        
+        $passwordResetManager->createPasswordResetTemp($email, $key, $expDate);
+
+
         //partie à mettre en place avec PHPMailer
-        $output ='<!DOCTYPE html>';
-        $output .='<html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Admin MNS</title></head>';
-        $output .='<body>';
-        $output .='<p>Dear user,</p>';
-        $output .='<p>Please click on the following link to reset your password.</p>';
-        $output .='<p>-------------------------------------------------------------</p>';
-        $output .='<p><a href="admin.tirepied.re/?area=security&controller=security&action=resetPassword&key='.$key.'&email='.$email.'">Réinitialiser le mot de passe</a></p>';		
-        $output .='<p>-------------------------------------------------------------</p>';
-        $output .='<p>Please be sure to copy the entire link into your browser.';
-        $output .= 'The link will expire after 1 hour for security reason.</p>';
-        $output .='<p>If you did not request this forgotten password email, no action is needed, your password will not be reset. However, you may want to log into your account and change your security password as someone may have guessed it.</p> ';  	
-        $output .='<p>Thanks,</p>';
-        $output .='<p>Admin MNS Team</p>';
-        $output .='</body>';
-        $output .='</html>';
-        $body = $output; 
-        $subject = "Password Recovery - Admin MNS";
-        
+        $output = '<!DOCTYPE html>';
+        $output .= '<html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Admin MNS</title></head>';
+        $output .= '<style>body{width:100%;height=100%;padding=12px;backgound-color:#151B36;color:#fff;}</style>';
+        $output .= '<body>';
+        $output .= '<p>Cher.e utilisateur.rice,</p>';
+        $output .= '<p>Merci de cliquer sur le lien suivant pour réinitialiser votre mot de passePlease click on the following link to reset your password.</p>';
+        $output .= '<p>-------------------------------------------------------------</p>';
+        $output .= '<p><a href="admin.tirepied.re/?area=security&controller=security&action=resetPassword&key=' . $key . '&email=' . $email . '">Cliquez-ici</a></p>';
+        $output .= '<p>-------------------------------------------------------------</p>';
+        $output .= "<p>ou de copier coller l'intégalité du lien dans votre barre de recherche";
+        $output .= 'Pour des raisons de sécurité, le lien expirera dans une heure</p>';
+        $output .= "<p>Si vous n'êtes pas à l'origine de cette demande de réinitialisation, aucune action n'est nécessaire, votre mot de passe ne sera pas réinitialisé. Cependant, nous vous conseillons de vous connecter à votre compte et de modifier votre mot de passe car celui-ci est peut être compromis</p> ";
+        $output .= '<p>Merci,</p>';
+        $output .= "<p>L'équipe Admin MNS</p>";
+        $output .= '</body>';
+        $output .= '</html>';
+        $body = $output;
+        $subject = "Réinitialisation de votre mot de passe - Admin MNS";
+
         $email_to = $email;
         $fromserver = "noreply@adminmns.com";
 
@@ -83,15 +85,10 @@ if (!$email) {
         $mail->AddAddress($email_to);
         $mail->send();
 
-        if (!$mail->Send()) 
-        {
+        if (!$mail->Send()) {
             echo "Mailer Error: " . $mail->ErrorInfo;
-        } 
-        else 
-        {
+        } else {
             $success = '<p>Un email vous a été envoyé avec les instructions pour réinitialiser votre mot de passe.</p>';
         }
-            
-    }   
-
+    }
 }
